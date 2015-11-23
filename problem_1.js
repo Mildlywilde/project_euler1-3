d9955a79612eed3eed23c33e5e7d446b5b7dae56
Jack WIlde <jackwilde630@gmail.com>
2015-11-23 14:23:21 -0500

    Solution for first problem

---
+++ problem_1.js
@@ -0,0 +1,10 @@
+var divider = function(input){
+	var sum = 0
+	for (var i = 1; i <= input; i ++){
+		if( (i % 3 === 0) || (i % 5 === 0)){
+			sum += i;
+		}
+	}
+	console.log(sum)
+}
+divider(1000)
